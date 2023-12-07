9221fee7799a0a9f9a2296330bf459a11792bb9c
Joseph Walker <joseph.walker@softwire.com>
2023-12-07 14:24:25 +0000

    form to create new todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -10,3 +10,9 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     return render_template("index.html", items=get_items())
+
+@app.route("/new", methods=["POST"])
+def new_todo():
+    add_item(request.form.get("item"))
+
+    return redirect("/")
